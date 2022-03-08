86e5c08af2c74f45552c144c0c24b81f5a1bc831
Guillermo Del Vecchio <guillermo.deve@gmail.com>
2022-03-08 02:10:30 -0300

    test: change border

--- src/designSystem/main.js
+++ src/designSystem/main.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Card = styled.div`
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   transition: 0.3s;
-  border-radius: 10%;
+  border-radius: 5%;
   width: 30rem;
   padding: 20px;
   margin-top: 10px;
